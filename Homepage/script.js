33d4275b495959493bcb4538920c2ba2ac3f8494
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-01-28 21:39:01 +0100

    homepage working

--- Homepage/script.js
+++ Homepage/script.js
@@ -5,58 +5,56 @@ document.getElementById("register").addEventListener("click", () => {
     departure: document.getElementById("departure").value,
     arrival: document.getElementById("arrival").value,
     date: document.getElementById("date").value,
-    time: document.getElementById("time").vlue,
+    time: document.getElementById("time").value,
+    itineraryType: "best",
   };
-  fetch("https://tickethack-backend-dusky.vercel.app/home", {
+
+  fetch("https://tickethack-backend-dusky.vercel.app/home/search", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(inputData),
   })
     .then((response) => response.json())
     .then((availableTrains) => {
-      console.log("availableTrains", availableTrains);
-      const result = availableTrains.result;
-      const trainList = availableTrains.trainList;
-      const errorMessage = availableTrains.error;
-      //Condition IF the result is True
-      if (result) {
-        for (const trains of trainList) {
-          const { departure, arrival, date, price, _id } = trains;
-          const departureTime = date.slice(11, 16);
-          // Appends the new Divs with all the trips for the date + time
-          document.getElementById("result").innerHTML += `
-          <div class="trainList">
-            <div class="trip">${departure} > ${arrival}</div>
-            <div class="departureTime">${departureTime}</div>
-            <div class="price">${price}€</div>
-            <button class="book-button" id=${_id}>Book</button>
-          </div>
-          `;
-        }
-        for (let i = 0; i < document.querySelectorAll(".book-button").length; i++) {
-          console.log(
-            'document.querySelectorAll(".book-button")',
-            document.querySelectorAll(".book-button")[i]
-          );
-          document.querySelectorAll(".book-button")[i].addEventListener("click", function () {
-            const id = this.id;
-            fetch(`https://tickethack-backend-dusky.vercel.app/home/book/${id}`, {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-            })
-              .then((response) => response.json())
-              .then((data) => {
-                window.location.assign("../Cart/cart.html");
-              })
-              .catch((error) => console.log(error));
-          });
-        }
-      } else {
+      const { result, allTrips, error } = availableTrains;
+      if (!result) {
         // Appends the error message that resulted from the route
         document.getElementById("result").innerHTML += `
-          <img src="../images/train.png" />
-          <h3>${errorMessage}</h3>
+        <img src="../images/train.png" />
+        <h3>${error}</h3>
         `;
+        return;
+      }
+
+      for (const trains of allTrips) {
+        const { departureStation, arrivalStation, departureDate, departureTime, fare, id } = trains;
+        document.getElementById("result").innerHTML += `
+          <div class="trainList">
+            <div class="trip">${departureStation} > ${arrivalStation}</div>
+            <div class="departureDate">${departureDate}</div>
+            <div class="departureTime">${departureTime}</div>
+            <div class="price">${fare}€</div>
+            <button class="book-button" id=${id}>Book</button>
+          </div>
+          `;
+      }
+      for (let i = 0; i < document.querySelectorAll(".book-button").length; i++) {
+        console.log(
+          'document.querySelectorAll(".book-button")',
+          document.querySelectorAll(".book-button")[i]
+        );
+        document.querySelectorAll(".book-button")[i].addEventListener("click", function () {
+          const id = this.id;
+          fetch(`https://tickethack-backend-dusky.vercel.app/home/book/${id}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              window.location.assign("../Cart/cart.html");
+            })
+            .catch((error) => console.log(error));
+        });
       }
     });
 });
